796a1e7767f33afc3bbb04ffe8ca0d02e07752f7
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-02-09 13:02:42 -0700

    removed itemStock

--- src/create/controller/Item.java
+++ src/create/controller/Item.java
@@ -4,14 +4,12 @@ public class Item
 {
 	public double itemPrice;
 	public String itemName;
-	public int itemStock;
 	public int itemIdentifier;
 	
-	public Item(double itemPrice, String itemName, int itemStock, int itemIdentifier)
+	public Item(double itemPrice, String itemName, int itemIdentifier)
 	{
 		this.itemPrice = itemPrice;
 		this.itemName = itemName;
-		this.itemStock = itemStock;
 		this.itemIdentifier = itemIdentifier;		
 	}
 	
@@ -26,19 +24,9 @@ public class Item
 		return itemName;
 	}
 	
-	int getItemStock()
-	{
-		return itemStock;
-	}
-	
 	int getItemIdentifier()
 	{
 		return itemIdentifier;
 	}
 	
-	//Will update the stock amount if changed
-	public void updateStock(int itemStock)
-	{
-		this.itemStock = itemStock;
-	}
 }
