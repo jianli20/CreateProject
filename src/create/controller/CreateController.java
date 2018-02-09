2d7bd5a3a84d10486b6943cc60cc189181b4e212
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-02-09 12:32:20 -0700

    Created Runner and start method

--- src/create/controller/CreateController.java
+++ src/create/controller/CreateController.java
@@ -2,5 +2,8 @@ package create.controller;
 
 public class CreateController
 {
-
+	public void start()
+	{
+	
+	}
 }
--- src/create/controller/CreateRunner.java
+++ src/create/controller/CreateRunner.java
@@ -2,5 +2,9 @@ package create.controller;
 
 public class CreateRunner
 {
-
+	public static void main(String [] args)
+	{
+	CreateController app = new CreateController();
+	app.start();
+	}
 }
