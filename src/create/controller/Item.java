a9221381e06e188534c7d5a911df56cc43f96e22
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-03-20 12:35:00 -0600

    Added item class code

--- src/create/controller/Item.java
+++ src/create/controller/Item.java
@@ -1,17 +1,52 @@
 package create.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Item
 {
+	private List<Item> mainList;
+	private List<Item> sideList;
+	private List<Item> drinkList;
 	public double itemPrice;
 	public String itemName;
 	public int itemAmount;
 	
-	public Item(double itemPrice, String itemName, int itemAmount)
+	public Item(double itemPrice, String itemName)
 	{
 		this.itemPrice = itemPrice;
 		this.itemName = itemName;
-		this.itemAmount = itemAmount;		
+		this.mainList = new ArrayList<Item>();
+		this.sideList = new ArrayList<Item>();
+		this.drinkList = new ArrayList<Item>();
+		
+		buildMain();
+		buildSide();
+		buildDrink();
 	}
+	
+	void buildMain()
+	
+	{
+		mainList.add(4.99 ,"Hamburger");
+		mainList.add(5.49, "Cheeseburger");
+		mainList.add(3.99, "Wrap");
+	}
+	
+	void buildSide()
+	{
+		sideList.add(2.99, "Fries");
+		sideList.add(1.49, "Salad");
+		sideList.add(2.99, "Soup");
+	}
+	
+	void buildDrink()
+	{
+		drinkList.add(2.00, "Coke");
+		drinkList.add(2.00, "Sprite");
+		drinkList.add(1.49, "Coffee");
+	}
+	
 	
 	//Getters
 	double getItemPrice()
@@ -22,11 +57,6 @@ public class Item
 	String getItemName()
 	{
 		return itemName;
-	}
-	
-	int getItemAmount()
-	{
-		return itemAmount;
 	}
 	
 }
