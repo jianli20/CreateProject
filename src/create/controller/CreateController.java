3aef57efefb8f40c8d2240ebf1ec198a1ebd5a48
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-03-20 12:34:39 -0600

    Controller class clode, popup display

--- src/create/controller/CreateController.java
+++ src/create/controller/CreateController.java
@@ -1,9 +1,28 @@
 package create.controller;
 
+import java.util.List;
+import create.view.CreateFrame;
+import create.controller.Item;
+
 public class CreateController
 {
+	private CreateController appController;
+	private CreateFrame appFrame;
+	private Item appItem;
+
 	public void start()
 	{
 	
 	}
+	
+	public CreateController()
+	{
+		appController = new CreateController();
+		appFrame = new CreateFrame(this);
+		appItem = new Item(0.0, "");
+		
+		
+	}	
 }
+
+
---
+++ src/create/view/PopupDisplay.java
@@ -0,0 +1,15 @@
+package create.view;
+
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+
+public class PopupDisplay
+{
+	private ImageIcon icon;
+	private String windowTitle;
+	
+	public PopupDisplay()
+	{
+	
+	}
+}
